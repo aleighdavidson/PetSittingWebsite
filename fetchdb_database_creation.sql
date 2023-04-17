e490a04a820754ad57f21ec20f7e4f44ff3252d0
Alex Davidson <alex.leigh.davidson@gmail.com>
2023-04-17 21:00:47 +0100

    updated id column names and photo data type

--- fetchdb_database_creation.sql
+++ fetchdb_database_creation.sql
@@ -3,7 +3,7 @@ create database fetchdb;
 use fetchdb;
 
 create table user(
-	user_id int auto_increment not null primary key,
+	id int auto_increment not null primary key,
     first_name varchar(50) not null,
     last_name varchar(50) not null,
     city varchar(50) not null,
@@ -15,25 +15,25 @@ create table user(
     );
 
 create table dog_type(
-	dog_type_id int auto_increment not null primary key,
+	id int auto_increment not null primary key,
     dog_type varchar(50) not null,
     description text not null
     );
 
 create table dog(
-	dog_id int auto_increment not null primary key,
+	id int auto_increment not null primary key,
     user_id int not null,
     dog_name varchar(50) not null,
-    dog_type_id int not null,
+    type_id int not null,
     description text,
-    foreign key(user_id) references user(user_id),
-    foreign key(dog_type_id) references dog_type(dog_type_id)
+    foreign key(user_id) references user(id),
+    foreign key(type_id) references dog_type(id)
     );
 
 create table dog_photos(
-	photo_id int auto_increment not null primary key,
+	id int auto_increment not null primary key,
     dog_id int not null,
-    photo blob not null,
-    foreign key(dog_id) references dog(dog_id)
+    photo varchar(260) not null,
+    foreign key(dog_id) references dog(id)
     );
     
