9f88e600a25b7a7fceae559bab94b00a849adbd6
Alex Davidson <alex.leigh.davidson@gmail.com>
2023-04-23 19:25:36 +0100

    added onclick redirect functionality for edit user and dog details, reference javascript file in header.html

---
+++ FetchApp/application/static/javascript_functions.js
@@ -0,0 +1,7 @@
+function redirectUser(id){
+window.location.href ='http://127.0.0.1:5000/edituser/'+ id;
+}
+
+function redirectDog(id){
+window.location.href ='http://127.0.0.1:5000/editdog/'+ id;
+}
